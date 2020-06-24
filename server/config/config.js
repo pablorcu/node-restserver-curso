4e9d31f9de4efb964756b952638eeaa94484322a
PabloC <pcampoverde73@gmail.com>
2020-06-23 21:07:19 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,23 @@
 //  Puerto
 //==============================
 process.env.PORT = process.env.PORT || 3000;
+
+//==============================
+//  Entorno
+//==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==============================
+//  Base de datos
+//==============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe-user:Ft4XuLa2qPsmAiNV@cluster0-0ppr3.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
+
+
+
